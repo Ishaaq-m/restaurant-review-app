b0339ddcefb70d18998f6bf848be935cf1fc2930
Ishaaq <ishaaqmalik879@gmail.com>
2022-07-25 12:57:32 +0100

    Created Profile Fragment

--- app/src/main/java/com/example/restaurantreviewapp/ProfileFragment.kt
+++ app/src/main/java/com/example/restaurantreviewapp/ProfileFragment.kt
@@ -5,17 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        (requireActivity() as MainActivity).supportActionBar?.title = getString(R.string.account)
+
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val emailText = view.findViewById<MaterialTextView>(R.id.user_email)
+
+        val logoutBtn = view.findViewById<MaterialButton>(R.id.logout_btn)
+
+        val user = auth.currentUser
+
+        if (user != null) {
+            emailText.text = user.email
+        }
+
+        return view;
     }
+
+
 
 }
