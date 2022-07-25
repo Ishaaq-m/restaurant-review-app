ef60e29b68d306ff9528c18910b7be3cac1b6cbf
Ishaaq <ishaaqmalik879@gmail.com>
2022-07-25 14:04:49 +0100

    Added login method to Login Activity

--- app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
+++ app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.restaurantreviewapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
@@ -48,7 +51,19 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun login(email: String, password: String) {
+        val view = findViewById<ViewGroup>(R.id.login_layout)
 
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else {
+                    Snackbar.make(view, getString(R.string.login_error_msg), Snackbar.LENGTH_SHORT).show()
+                }
+            }
     }
 
 }
