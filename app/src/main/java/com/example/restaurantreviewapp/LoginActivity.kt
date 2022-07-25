100fedb3c4d63782def3fb0604049eacba675bd1
Ishaaq <ishaaqmalik879@gmail.com>
2022-07-25 13:59:22 +0100

    Added field validation for login page

--- app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
+++ app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
@@ -18,6 +18,37 @@ class LoginActivity : AppCompatActivity() {
 
         val loginBtn = findViewById<MaterialButton>(R.id.login_btn)
         val cancelBtn = findViewById<MaterialButton>(R.id.cancel_btn)
+
+        loginBtn.setOnClickListener {
+            validateFields()
+        }
+
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun validateFields() {
+        val emailInput = findViewById<TextInputEditText>(R.id.email_input)
+        val passwordInput = findViewById<TextInputEditText>(R.id.password_input)
+
+        val email = emailInput.text.toString().trim()
+        val password = passwordInput.text.toString().trim()
+
+        if (email.isEmpty()) {
+            emailInput.error = getString(R.string.email_required_msg)
+        } else if (password.isEmpty()) {
+            passwordInput.error = getString(R.string.password_required_msg)
+        } else if (password.length < 6) {
+            passwordInput.error = getString(R.string.password_length_msg)
+        }
+        else {
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+
     }
 
 }
