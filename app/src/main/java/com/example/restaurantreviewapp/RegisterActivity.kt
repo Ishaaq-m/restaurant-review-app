be7d316cdb5792d1c4ddcca0314787aa89d64d3e
Ishaaq <ishaaqmalik879@gmail.com>
2022-07-25 10:22:46 +0100

    Added field validation for register page

--- app/src/main/java/com/example/restaurantreviewapp/RegisterActivity.kt
+++ app/src/main/java/com/example/restaurantreviewapp/RegisterActivity.kt
@@ -19,5 +19,36 @@ class RegisterActivity : AppCompatActivity() {
         val registerBtn = findViewById<MaterialButton>(R.id.register_btn)
         val cancelBtn = findViewById<MaterialButton>(R.id.cancel_btn)
 
+        registerBtn.setOnClickListener {
+            validateFields()
+        }
+
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun validateFields() {
+        val emailInput = findViewById<TextInputEditText>(R.id.email_input)
+        val passwordInput = findViewById<TextInputEditText>(R.id.password_input)
+
+        val email = emailInput.text.toString().trim()
+        val password = emailInput.text.toString().trim()
+
+        if (email.isEmpty()) {
+            emailInput.error = getString(R.string.email_required_msg)
+        } else if (password.isEmpty()) {
+            passwordInput.error = getString(R.string.password_required_msg)
+        } else if (password.length < 6) {
+            passwordInput.error = getString(R.string.password_length_msg)
+        }
+        else {
+            register(email, password)
+        }
+
+    }
+
+    private fun register(email: String, password: String) {
+
     }
 }
