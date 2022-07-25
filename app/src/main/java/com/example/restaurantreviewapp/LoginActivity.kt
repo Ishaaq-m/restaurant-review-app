0c2c9d1c10bbb9a411c0fe3d736da7a5d8f89963
Ishaaq <ishaaqmalik879@gmail.com>
2022-07-25 13:54:10 +0100

    Created Login Activity

--- app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
+++ app/src/main/java/com/example/restaurantreviewapp/LoginActivity.kt
@@ -2,10 +2,22 @@ package com.example.restaurantreviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val loginBtn = findViewById<MaterialButton>(R.id.login_btn)
+        val cancelBtn = findViewById<MaterialButton>(R.id.cancel_btn)
     }
+
 }
